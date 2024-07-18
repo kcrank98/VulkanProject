5fa5cafcf877137e81b7498b16660ac755dcc08c
kcrank98 <138240807+kcrank98@users.noreply.github.com>
2024-07-18 17:23:36 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -744,9 +744,11 @@ public:
 		vlk.GetSwapchainCurrentImage(currentBuffer);
 		GvkHelper::write_to_buffer(device,
 			uniformDatas[currentBuffer], &sceneData, sizeof(sceneData));
-		for (int i = 0; i < swapChainCount; ++i) {
+		for (int i = 0; i < 1; ++i) {
 			GvkHelper::write_to_buffer(device,
 				storageDatas[currentBuffer], &perFrame[i], sizeof(INSTANCE_DATA));
+			GvkHelper::write_to_buffer(device,
+				storageDatas[currentBuffer + 1], &perFrame[i + 1], sizeof(INSTANCE_DATA));
 		}
 	}
 
